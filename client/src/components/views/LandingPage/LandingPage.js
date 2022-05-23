97e9e1aa6c59150f81630b601217a1e13338e9de
formal369 <73514392+formal369@users.noreply.github.com>
2022-05-23 19:51:44 +0900

    영화 상세정보 보기 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -54,6 +54,7 @@ const LandingPage = (props) => {
                 {Movies && Movies.map((movie, index) => (
                     <React.Fragment key={index}>
                         <GridCards
+                            landingPage
                             image={movie.poster_path ?
                                 `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
                             movieId={movie.id}
