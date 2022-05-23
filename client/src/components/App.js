10584eb0fe7b0ebbbad32c72a86398851db37090
formal369 <73514392+formal369@users.noreply.github.com>
2022-05-23 19:51:05 +0900

    영화 상세정보 보기 구현

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,12 +6,14 @@ import NavBar from "./views/NavBar/NavBar";
 import LandingPage from './views/LandingPage/LandingPage';
 import LoginPage from './views/LoginPage/LoginPage';
 import RegisterPage from './views/RegisterPage/RegisterPage';
+import MovieDetail from './views/MovieDetail/MovieDetail';
 
 
 function App() {
   const AuthLandingPage = Auth(LandingPage, null);
   const AuthLoginPage = Auth(LoginPage, false);
   const AuthRegisterPage = Auth(RegisterPage, false);
+  const AuthMovieDetail = Auth(MovieDetail, null);
   
   return (
     <>
@@ -21,6 +23,7 @@ function App() {
           <Route exact path="/" element={<AuthLandingPage />} />
           <Route exact path="/login" element={<AuthLoginPage />} />
           <Route exact path="/register" element={<AuthRegisterPage />} />
+          <Route exact path="/movie/:movieId" element={<AuthMovieDetail />} />
         </Routes>
       </Router>
     </>
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 const LandingPage = (props) => {
 
     const [Movies, setMovies] = useState([]);
     const [MainMovieImage, setMainMovieImage] = useState(null);
+    const [Currentpage, setCurrentpage] = useState(0);
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-
+        fetchMovies(endpoint)
+        
+    }, [])
+    
+    const fetchMovies = (endpoint) => {
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
             console.log(response.results)
-            setMovies([response.results])
+            setMovies([...Movies, ...response.results])
             setMainMovieImage(response.results[0])
+            setCurrentpage(response.page)
         })
-    }, [])
+    }
+    
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${Currentpage+1}`;
+        fetchMovies(endpoint)
+    }
 
 
     return (
@@ -37,11 +50,25 @@ const LandingPage = (props) => {
                 <hr />
 
                 {/* Movie Grid Cards */}
+                <Row gutter={[16, 16]}>
+                {Movies && Movies.map((movie, index) => (
+                    <React.Fragment key={index}>
+                        <GridCards
+                            image={movie.poster_path ?
+                                `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                            movieId={movie.id}
+                            movieName={movie.original_title}
+                        />
+
+                    </React.Fragment>
+                ))}
+
+                </Row>
 
             </div>
 
             <div style= {{ display: 'flex', justifyContent: 'center' }}>
-                <button>Load More</button>
+                <button onClick={loadMoreItems}>Load More</button>
             </div>
         </div>
     )
