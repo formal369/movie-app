5331bae6e872410bed75b78cab42d43ccf1447d0
formal369 <73514392+formal369@users.noreply.github.com>
2022-05-23 17:00:55 +0900

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '4606678e1172c3d7f13e0a684e5b9966';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
